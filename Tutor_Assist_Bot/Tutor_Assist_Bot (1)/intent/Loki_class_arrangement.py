5c2c9474141ba7bf798056df2758886da4ff96df
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2022-08-25 14:07:54 +0800

    update msc

--- Tutor_Assist_Bot/Tutor_Assist_Bot (1)/intent/Loki_class_arrangement.py
+++ Tutor_Assist_Bot/Tutor_Assist_Bot (1)/intent/Loki_class_arrangement.py
@@ -40,166 +40,241 @@ def getResult(inputSTR, utterance, args, resultDICT):
     if utterance == "[禮拜天][可以]幫[XX]上課嗎":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[0]
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
         resultDICT["class_arrangement"]["Course/Student"] = args[2]
         pass
 
     if utterance == "[禮拜天][可以]幫[弟弟]上課嗎":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[0]
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
         resultDICT["class_arrangement"]["Course/Student"] = args[2]
         pass
 
     if utterance == "延[後][一]小時半":
         resultDICT["intentLIST"].append("class_arrangement")
+        resultDICT["class_arrangement"]["AlterTime"] = "unknown"
         resultDICT["class_arrangement"]["EarlyOrLate"] = "延後"
         resultDICT["class_arrangement"]["AlterTimeSpan"] = args[1]+"小時半"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "延[後][半個][小時]":
         resultDICT["intentLIST"].append("class_arrangement")
+        resultDICT["class_arrangement"]["AlterTime"] = "unknown"
         resultDICT["class_arrangement"]["EarlyOrLate"] = "延後"
         resultDICT["class_arrangement"]["AlterTimeSpan"] = args[1]+args[2]
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "延[後][半小時]":
         resultDICT["intentLIST"].append("class_arrangement")
+        resultDICT["class_arrangement"]["AlterTime"] = "unknown"
         resultDICT["class_arrangement"]["EarlyOrLate"] = "延後"
         resultDICT["class_arrangement"]["AlterTimeSpan"] = args[1]
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "延[後]到[8].":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[1]
         resultDICT["class_arrangement"]["EarlyOrLate"] = "延後"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "延[後]到[八點]":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[1]
         resultDICT["class_arrangement"]["EarlyOrLate"] = "延後"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "提早[一]小時半":
         resultDICT["intentLIST"].append("class_arrangement")
+        resultDICT["class_arrangement"]["AlterTime"] = "unknown"
         resultDICT["class_arrangement"]["EarlyOrLate"] = "提早"
         resultDICT["class_arrangement"]["AlterTimeSpan"] = args[1]+"小時半"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "提早[半個][小時]":
         resultDICT["intentLIST"].append("class_arrangement")
+        resultDICT["class_arrangement"]["AlterTime"] = "unknown"
         resultDICT["class_arrangement"]["EarlyOrLate"] = "提早"
         resultDICT["class_arrangement"]["AlterTimeSpan"] = args[1]+args[2]
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "提早[半小時]":
         resultDICT["intentLIST"].append("class_arrangement")
+        resultDICT["class_arrangement"]["AlterTime"] = "unknown"
         resultDICT["class_arrangement"]["EarlyOrLate"] = "提早"
         resultDICT["class_arrangement"]["AlterTimeSpan"] = args[1]
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "提早到[8].":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[0]
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
         resultDICT["class_arrangement"]["EarlyOrLate"] = "提早"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "提早到[八點]":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[0]
         resultDICT["class_arrangement"]["EarlyOrLate"] = "提早"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "改[8].":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[0]
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "改[XX]的時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = args[0]
         pass
 
     if utterance == "改[八點]":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[0]
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "改[週四]的時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = args[0]
         pass
 
     if utterance == "改一下[XX]的時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
         resultDICT["class_arrangement"]["Course/Student"] = args[0]
         pass
 
     if utterance == "改一下上課時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "改一下時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "改上課時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "改到[8].":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[0]
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "改到[八點]":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = args[0]
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "改時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "討論[XX]的時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
         resultDICT["class_arrangement"]["Course/Student"] = args[0]
         pass
 
     if utterance == "討論[週四]的時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = args[0]
         pass
 
     if utterance == "討論上課時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "調整[XX]的時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
         resultDICT["class_arrangement"]["Course/Student"] = args[0]
         pass
 
     if utterance == "調整[週四]的時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = args[0]
         pass
 
     if utterance == "調整上課時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     if utterance == "調整時間":
         resultDICT["intentLIST"].append("class_arrangement")
         resultDICT["class_arrangement"]["AlterTime"] = "unknown"
+        resultDICT["class_arrangement"]["EarlyOrLate"] = "unknown"
+        resultDICT["class_arrangement"]["AlterTimeSpan"] = "unknown"
+        resultDICT["class_arrangement"]["Course/Student"] = "unknown"
         pass
 
     return resultDICT
